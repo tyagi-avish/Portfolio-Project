9e90ab7668fb4c78f1ebe6e14086d731a853c9e4
Avish Tyagi <tyagiavish04@gmail.com>
2025-02-24 23:00:00 +0530

    added some keyframes and javascript

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-var typed =new typed(".text", {
-    Strings:["Frontened Developer" , "youtuber" , "Web Developer"],
+var typed = new Typed(".text", {
+    strings:["Frontened Developer" , "youtuber" , "Web Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop: true
 });
+
+
+
